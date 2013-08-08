ddb80a39b0af32b11131adb40ee379dc3da4d1aa
Jan Stette <jan.stette@gmail.com>
2013-08-08 23:31:28 +0100

    Added repl functions, not working yet

--- src/tiny_lisp/core.clj
+++ src/tiny_lisp/core.clj
@@ -11,9 +11,12 @@
       (catch Exception e#
         (try-or ~@forms)))))
 
-(defn tokenize [str]
-  (filter (fn [s] (> (count s) 0)) 
-    (string/split (replace str #"([\(\)])" " $0 ") #"\s")))
+(defn eval [expr]
+      ; TODO - a few more details needed here.
+      (.toString expr))
+
+(defn parse [tokens]
+  tokens) ; // TODO: that's not quite right is it...
 
 (defn parse-atom [str]
   (cond 
@@ -24,3 +27,15 @@
             (Double/parseDouble str) 
             str)))
 
+(defn tokenize [str]
+  (remove #(blank? %) 
+    (string/split (replace str #"([\(\)])" " $0 ") #"\s")))
+
+(defn print-result [v]
+  (println v)) ; TODO: Render valid Lisp string.
+
+(defn repl []
+  (doseq [line (line-seq (java.io.BufferedReader. *in*))] 
+    (print-result (eval (tokenize (parse line))))
+    )
+  )
