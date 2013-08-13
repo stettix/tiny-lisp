28af7279c216e7d63d3fbcea08575bb17d0cad33
Jan Stette <jan.stette@gmail.com>
2013-08-13 10:27:41 +0100

    Changed parser to generate vectors. eval still completely broken.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject tiny-lisp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple Lisp interpreter"
+  :url "http://www.janvsmachine.net"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [net.mikera/cljunit "0.2.0"]])
+                 [net.mikera/cljunit "0.2.0"]
+                 [org.clojure/core.match "0.2.0-rc5"]])
--- src/tiny_lisp/core.clj
+++ src/tiny_lisp/core.clj
@@ -1,5 +1,6 @@
 (ns tiny-lisp.core
-  (require [clojure.string :as string]))
+  (require [clojure.string :as string])
+  (use [clojure.core.match :only (match)]))
 
 (defmacro try-or
   "See http://clj-me.cgrand.net/2009/01/08/try-or-or-try-try-else-or-else-try/" 
@@ -17,15 +18,23 @@
 
 ; [single expression, environment map] -> [result of evaluation, updated environment map]
 (defn eval-exp [expr env]
-  (def op (first expr))
-  (def args (rest expr))
-  (println op ":" args)
-  (cond
-    (string? expr) [expr env] ; TODO: Look up symbol in env.
-    (= "q" op) (eval-exp args env)
-    (= "atom?" op) (not (seq? (eval-exp args env)))
-  ; TODO - a few more details needed here...
-    :else [expr env]))
+  (match [expr]
+    [(s :guard string?)] (get env expr)
+    [[ (:or "q" "quote") & args ]] args
+    [[ "atom" & args ]] (not (seq? (eval-exp args env)))
+    :else expr)
+)
+
+;(defn eval-exp [expr env]
+;  (let [op (first expr)
+;        args (rest expr)]
+;    (println op ":" args)
+;    (cond
+;      (string? expr) (get env expr)
+;      (= "q" op) (eval-exp args env)
+;      (= "atom?" op) (not (seq? (eval-exp args env)))
+;      ; TODO - a few more details needed here...
+;      :else [expr env])))
 
 (defn parse-atom [str](eval-exp '("atom?" 42) {})
   (cond 
@@ -40,20 +49,20 @@
 (defn parse-list [tokens usedTokenCount aggr]
   (cond
     (empty? tokens) (throw (IllegalArgumentException. "Missing ')'"))
-    (= ")" (first tokens)) [(reverse aggr) (inc usedTokenCount)]
+    (= ")" (first tokens)) [aggr (inc usedTokenCount)]
     (= "(" (first tokens)) (do
                              (let [[nestedList, consumedTokens]
-                               (parse-list (rest tokens) 1 '())]
+                               (parse-list (rest tokens) 1 [])]
                                (parse-list 
                                  (drop consumedTokens tokens) 
                                  (+ usedTokenCount consumedTokens) 
-                                 (cons nestedList aggr))))
-    :else (recur (rest tokens) (inc usedTokenCount) (cons (parse-atom (first tokens)) aggr))
+                                 (conj aggr nestedList))))
+    :else (recur (rest tokens) (inc usedTokenCount) (conj aggr (parse-atom (first tokens))))
     ))
 
 ; list of tokens -> list of parsed expressions.
 (defn parse [tokens]
-  (get (parse-list (concat tokens '(")")) 0 '()) 0))
+  (get (parse-list (concat tokens [")"]) 0 []) 0))
 
 (defn tokenize [str]
   (remove #(string/blank? %) 
